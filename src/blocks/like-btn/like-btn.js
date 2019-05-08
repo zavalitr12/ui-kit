8a581a900ce05b27ac860706a3ad475d5616c625
zavalitr12 <zavalitr@mail.ru>
2019-05-09 00:18:42 +0500

    fixed like-btn js implementation

--- src/blocks/like-btn/like-btn.js
+++ src/blocks/like-btn/like-btn.js
@@ -1,7 +1,26 @@
 $(".like-btn").click(function(e){
-    $(".like-btn").toggleClass("like-btn_disabled");
-    if($(".like-btn__icon").text()=="favorite_border")
-        $(".like-btn__icon").text("favorite");
-    else
-        $(".like-btn__icon").text("favorite_border");
+    
+    var likes =  $(this).children('.like-btn__likes').text();
+
+    if($('.like-btn_disabled').length!=0){
+        
+        likes++;
+
+        $('.like-btn__likes').text(likes);
+
+        $('.like-btn__icon').text('favorite');
+
+        $('.like-btn').removeClass('like-btn_disabled');
+    }
+    else{
+        
+        likes--;
+
+        $('.like-btn__likes').text(likes);
+
+        $('.like-btn__icon').text('favorite_border');
+
+        $('.like-btn').addClass('like-btn_disabled');
+    }
+    
 });
